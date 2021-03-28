cbbb958c01ce6429334eea287a910768f68020a9
Kateryna Kochubei <kochubej.katerina@gmail.com>
2021-03-28 22:51:55 +0300

    customized swiper

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 let copyright = document.querySelector('.copyright-year');
 copyright.innerHTML =  new Date().getFullYear();
+
+const mySwiper = new Swiper('.swiper-container', {
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  navigation: {
+    nextEl: '.swiper-button-next--custom',
+    prevEl: '.swiper-button-prev--custom',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+  }
+})
